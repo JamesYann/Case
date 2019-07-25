c01ea48b60b572e050277c1aa33a1d702e96e7bc
amy <amyis0438@gmail.com>
2019-07-25 15:11:33 +0800

    dialog連接heroku
    測試的dialog及heroku，接下來要試試如何將資料回覆給linebot使用者

---
+++ dialog連接heroku/0725/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const express = require('express')
+const { WebhookClient } = require('dialogflow-fulfillment')
+const app = express()
+
+app.post('/dialogflow', express.json(), (req, res) => {
+    //------------------------------------
+    // 處理請求/回覆的Dialogflow代理人
+    //------------------------------------  
+    const agent = new WebhookClient({request: req, response: res})
+
+
+    //------------------------------------
+    // 處理歡迎意圖
+    //------------------------------------     
+    function welcome(){
+        agent.add('歡迎你!!!');
+    }
+
+    //------------------------------------
+    // 設定對話中各個意圖的函式對照
+    //------------------------------------
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    agent.handleRequest(intentMap);
+})
+
+
+//----------------------------------------
+// 監聽3000埠號, 
+// 或是監聽Heroku設定的埠號
+//----------------------------------------
+var server = app.listen(process.env.PORT || 3000, function() {
+    const port = server.address().port;
+    console.log("正在監聽埠號:", port);
+});
