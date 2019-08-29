a77273cd21dd89fe223f62b6a0637ebae7f32e04
JamesYan <james70937@gmail.com>
2019-08-29 15:31:04 +0800

    10736025_0827

---
+++ 10736025_0829/app/utility/select_schedule.js
@@ -0,0 +1,33 @@
+
+'use strict';
+
+//引用操作資料庫的物件
+const query = require('./asyncDB');
+
+
+//------------------------------------------
+// 新增收藏資料
+//------------------------------------------
+var selectSchedule = async function(group_id){
+    //存放結果
+    let result;  
+
+    //讀取資料庫
+    await query('select * from schedule where group_id=$1 ', ['Cfec095b130f619f66b26fe22e4629a61'])
+        .then((data) => {
+            if(data.rows.length>0){
+                result=data;
+            }else{
+                result=-1;//找不到資料
+            }
+        }, (error) => {
+            result = -9;  //執行錯誤
+        });
+
+    //回傳執行結果
+    return result;  
+}
+//------------------------------------------
+
+//匯出
+module.exports = {selectSchedule};
